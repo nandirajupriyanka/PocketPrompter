6980d3c764ec8915c0712578f990c63484e3cfb1
nandirajupriyanka <nandirajupriyanka@gmail.com>
2018-04-03 22:22:34 -0500

    feat: Add version name in settings
    - Add version name in SettingsActivity.java

--- app/src/main/java/com/priyankanandiraju/pocketprompter/SettingsActivity.java
+++ app/src/main/java/com/priyankanandiraju/pocketprompter/SettingsActivity.java
@@ -1,22 +1,32 @@
 package com.priyankanandiraju.pocketprompter;
 
+import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    @BindView(R.id.versionName)
+    TextView versionName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ButterKnife.bind(this);
 
         ActionBar actionBar = this.getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        String currentVersionName = getString(R.string.version_name, BuildConfig.VERSION_NAME);
+        versionName.setText(currentVersionName);
     }
 
     @Override
